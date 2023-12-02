d79e433324fbfa1462c5f045dfbe91bb72145383
Lorenzo Gutierrez <lorenzoplaypereira@gmail.com>
2023-12-01 21:00:00 -0300

    alteração em alguns arquivos

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,35 @@
-const gulp = require('gulp');
+const gulp = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+const sourcemaps = require('gulp-sourcemaps')
+const uglify = require('gulp-uglify')
+const obfuscate = require('gulp-obfuscate')
+const imagemin = require('gulp-imagemin')
 
-function funcaoPadrao(callback) {
-    console.log("Executando via gulp");
-    callback();
+function comprimeimagens() {
+    return gulp.src('./source/images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./build/images'))
 }
 
-function dizOi(callback) {
-    console.log("olá gulp");
-    dizTchau();
-    callback();
+function comprimeJavaScript() {
+    return gulp.src('./source/scripts/*.js')
+        .pipe(uglify())
+        .pipe(obfuscate())
+        .pipe(gulp.dest('./build/scripts'))
 }
 
-function dizTchau() {
-    console.log("Tchau Gulp");
+function compilaSass() {
+    return gulp.src('./source/styles/main.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+        outputStyle: 'compressed'
+    }))
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(gulp.dest('./build/styles'))
 }
 
-exports.default = gulp.series(funcaoPadrao, dizOi);
-exports.dizOi = dizOi;
+exports.default = function() {
+    gulp.watch('./source/styles/*.scss', { ignoreInitial: false}, gulp.series(compilaSass))
+    gulp.watch('./source/scripts/*.js', { ignoreInitial: false}, gulp.series(comprimeJavaScript))
+    gulp.watch('./source/images/*', { ignoreInitial: false}, gulp.series(comprimeimagens))
+}
